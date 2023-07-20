aa2ccee6c18d1c52c378c1dbf6103270513fa9ce
K A Ganesh Kumar <117643249+ganeshkumarka@users.noreply.github.com>
2023-07-20 15:46:50 +0530

    Add files via upload

--- JAVAEXPERIMENTS/src/main/java/Exp11/AbstractDemo.java
+++ JAVAEXPERIMENTS/src/main/java/Exp11/AbstractDemo.java
@@ -9,5 +9,66 @@ package Exp11;
  * @author cusat
  */
 public class AbstractDemo {
+     public static void main(String[] args) {
+        //BankAccount ob=new BankAccount()
+    }
+}
+abstract class BankAccount{
+    int account_no;
+    double balance;
+
+    public BankAccount(int account_no, double balance) {
+        this.account_no = account_no;
+        this.balance = balance;
+    }
+    
+    
+    
+    public void deposit(double amount){
+        balance += amount;
+    }
+    
+    abstract public void withdraw(double amount);
+    
+}
+
+class SavingAccount extends BankAccount{
+    
+    public SavingAccount(int account_no, double balance) {
+        super(account_no, balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        
+        if(balance - 500 >= amount){
+            balance -= amount;
+        }
+        else
+        {
+            System.out.println("Insufficient Balance");
+        }
+       
+    }
+    
+    
+}
+
+class CurrentAccount extends BankAccount{
+    
+    public CurrentAccount(int account_no, double balance) {
+        super(account_no, balance);
+    }
+    
+    public void withdraw(double amount){
+        if(balance + 1000 >= amount){
+            balance -= amount;
+        }
+        else
+        {
+            System.out.println("Insufficient Balance");
+        }
+    }
+    
     
 }
--- JAVAEXPERIMENTS/src/main/java/Exp11/FinalUsage.java
+++ JAVAEXPERIMENTS/src/main/java/Exp11/FinalUsage.java
@@ -9,5 +9,30 @@ package Exp11;
  * @author cusat
  */
 public class FinalUsage {
+      public static void main(String[] args) {
+        
+        final double pie = 3.14;
+        System.out.println("Pie : "+pie);
+        //pie = 3.41; make an error
+        System.out.println("Pie : "+pie);
+        
+    }
+}
+
+class Simple{
+
+    final public void greet()
+    {
+            System.out.println("Good Morning !");
+    }
+}
+final class Complex extends Simple{
+   
+    //public void greet(){} cannot overide a final method 
+    
+    public void greetings()
+    {
+        System.out.println("Hello! How are you ?");
+    }
     
 }
